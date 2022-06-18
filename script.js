043558911aaae050d90daa0f26cfbcef83050455
naufan17 <muhammadnaufan719@yahoo.com>
2022-06-18 11:20:54 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let click = 1;
+
+function cekFizzBuzz(){
+    let number = document.getElementById("inputNumber").value;
+    let text = document.createElement("p");
+    let result;
+
+    if(click == 1){
+        result = number;
+    }else{
+        if(number == 0){
+            result = "Invalid Number"; 
+        }else if(number%5 == 0 && number%3 == 0){
+            result = "FizzBuzz!";
+        }else if(number%3 == 0){
+            result = "Buzz!";
+        }else if(number%5 == 0){
+            result = "Fizz!";
+        }else{
+            result = number;
+        }
+    }
+    
+    // document.getElementById("result").innerHTML = result;
+
+    text.innerText = result;
+    document.body.appendChild(text);
+
+    console.log(result);
+    // console.log(click);
+    click += 1;
+}
